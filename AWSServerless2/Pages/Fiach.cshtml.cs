7ce19e53cddc584659cc8c046b273f7395bdac8e
fiach <fiach.reid@gmail.com>
2020-06-11 12:45:07 +0200

    Initial commit

---
+++ AWSServerless2/Pages/Fiach.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWSServerless2.Pages
+{
+    public class FiachModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
